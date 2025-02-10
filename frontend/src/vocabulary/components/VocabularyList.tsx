53c262bd1cae40f8702b86e2f379884f328251fa
LTKSK <ltksk0714@gmail.com>
2025-02-10 22:55:57 +0900

    tanstack router 追加

---
+++ frontend/src/vocabulary/components/VocabularyList.tsx
@@ -0,0 +1,12 @@
+export const VocabularyList = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Vocabulary List</h1>
+      <ul>
+        <li>Word 1</li>
+        <li>Word 2</li>
+        <li>Word 3</li>
+      </ul>
+    </div>
+  );
+};
