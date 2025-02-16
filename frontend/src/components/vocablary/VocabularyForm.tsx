0e298d5c993224c92abfa2ac5b7fc4f5c6e7995c
LTKSK <ltksk0714@gmail.com>
2025-02-16 10:41:44 +0900

    mod: file名を-prefixを付ける形に変更

--- frontend/src/components/vocablary/VocabularyForm.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/vocabulary/components/VocabularyForm')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/vocabulary/components/VocabularyForm"!</div>
-}
--- frontend/src/routes/vocablary.tsx
+++ frontend/src/routes/vocablary.tsx
@@ -1,9 +1,25 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+type Vocabulary = {
+  word: string;
+  definition: string;
+};
+
 export const Route = createFileRoute("/vocablary")({
   component: Vocabulary,
+  loader: () =>
+    new Promise((resolve) =>
+      setTimeout(() => {
+        const vocablary: Vocabulary = {
+          word: "vocabulary",
+          definition: "the body of words used in a particular language.",
+        };
+        resolve(vocablary);
+      }, 1000)
+    ),
 });
 
-function Vocabulary() {
+function Vocabulary(vocablary: Vocabulary) {
+  console.log("vocablary", vocablary);
   return <div>Hello "vocabulary"!</div>;
 }
